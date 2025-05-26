c7d230771e6cb46aef9e1aa247346c7bac8abb78
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-05-26 15:09:48 +0700

    chore: cleaning up files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,14 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
 import dotenv from 'dotenv';
-import api, { protectedRoutes } from '../routes/index.ts';
+import api from '../routes/index.ts';
+
 dotenv.config();
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const app = new Hono();
 
-// set up middleware
+// Set up middleware
 app.use('*', logger());
 app.use('*', cors({
   origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
@@ -23,7 +24,6 @@ app.get('/', (c) => {
   return c.json({ status: 'success', message: 'PinPoint API is running' });
 });
 
-// hook up all our routes
 app.route('', api);
 
 console.log(`Server is running on port ${port}`);

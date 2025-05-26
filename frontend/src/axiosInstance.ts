84a1348e5f8b9f4437cfede0b2e0ef01397226e9
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-05-26 15:59:45 +0700

    merge: :(

--- frontend/src/axiosInstance.ts
+++ frontend/src/axiosInstance.ts
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-	baseURL: 'http://localhost:3000', 
+    baseURL: 'http://localhost:3000', 
+    withCredentials: true, // <-- Add this line
+});
+
+// Add this to your axiosInstance.js
+Axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export { Axios };
